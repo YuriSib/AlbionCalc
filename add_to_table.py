918e264e04b27f7bec9108793b5603e1bdac997c
User <itproj31@gmail.com>
2024-06-27 22:57:24 +0300

    2

--- add_to_table.py
+++ add_to_table.py
@@ -1,14 +1,27 @@
 import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 
-def add_block(material_data: [()]):
+def row_col_to_a1(row, col):
+    """Преобразование координат строки и столбца в формат 'A1'."""
+    letter = ''
+    while col > 0:
+        col, remainder = divmod(col - 1, 26)
+        letter = chr(65 + remainder) + letter
+    return f'{letter}{row}'
+
+
+def add_value(material_data: [()]):
     gc = gspread.service_account(filename='creds.json')
-    sheet = gc.open("таблица расчета").get_worksheet(1)
+    # sheet = gc.open("таблица расчета").get_worksheet(1)
+    sheet = gc.open("Тест").sheet1
 
-    start_row = 80
+    for value in material_data:
+        row = value['row']
+        column = value['column']
 
-    last_min = sheet.cell(9, 3).value
-    return last_min
+        sheet.update_cell(row=row, col=column, value=value['value'])
+
 
     # if current_date in date_list:
     #     cur_row = date_list.index(current_date)
@@ -35,7 +48,4 @@ def add_block(material_data: [()]):
     # sheet.update_cell(row=cur_row + 1, col=user_col, value=int(last_min) + min_cost)
     # sheet.update_cell(row=cur_row + 1, col=user_col + 1, value=int(last_max) + max_cost)
     # sheet.update_cell(row=cur_row + 1, col=user_col + 2, value=last_table + table_name)
-
-
-print(add_block())
 
--- scrapper.py
+++ scrapper.py
@@ -6,7 +6,16 @@ import json
 import time
 
 from fake_useragent import UserAgent
-from playwright.sync_api import Playwright, sync_playwright
+from playwright.sync_api import sync_playwright
+
+
+xpath_resources = {
+    "metal": '''//*[@id="__layout"]/div/div/section/div[3]/div[2]/div/div[1]''',
+    "leather": '''//*[@id="__layout"]/div/div/section/div[3]/div[2]/div/div[2]''',
+    "cloth": '''//*[@id="__layout"]/div/div/section/div[3]/div[2]/div/div[3]''',
+    "wood": '''//*[@id="__layout"]/div/div/section/div[3]/div[2]/div/div[4]''',
+    "stone": '''//*[@id="__layout"]/div/div/section/div[3]/div[2]/div/div[5]'''
+}
 
 
 def get_html(xpath_dict: dict):
@@ -30,18 +39,46 @@ def get_html(xpath_dict: dict):
     return html_dict
 
 
-xpath_resources = {
-    "metal": '''//*[@id="__layout"]/div/div/section/div[3]/div[2]/div/div[1]''',
-    "leather": '''//*[@id="__layout"]/div/div/section/div[3]/div[2]/div/div[2]''',
-    "cloth": '''//*[@id="__layout"]/div/div/section/div[3]/div[2]/div/div[3]''',
-    "wood": '''//*[@id="__layout"]/div/div/section/div[3]/div[2]/div/div[4]''',
-    "stone": '''//*[@id="__layout"]/div/div/section/div[3]/div[2]/div/div[5]'''
-}
+def material_pars(material_html) -> list:
+    metal_list = material_html.find_all('div', {'class': 'item-row'})
+    material_data = []
+    col_num = 1
+    row_num = 1
+    for div in metal_list:
+        data = div.find_all('div', {'data-v-46ecdbca': True})
+        data_list = [block for block in data if len(block.get_text(strip=True)) < 10]
 
-html_dict = get_html(xpath_resources)
-metal_html = html_dict.get('metal')
-metal_list = metal_html.find_all('div', {'class': 'item-row'})
-for i in metal_list:
-    print(i)
-    print('__________________________________________________________________________\n'*4)
+        for data in data_list:
+            if 'class="success"' in str(data):
+                time_ = {'value': data.get_text(strip=True), 'row': row_num, 'column': col_num + 1}
+                material_data.append(time_)
+                print(time_)
+                row_num += 1
+            else:
+                cost = {'value': data.get_text(strip=True), 'row': row_num, 'column': col_num}
+                material_data.append(cost)
+                print(cost)
 
+            if data.get_text(strip=True) == '-':
+                print('**************************************')
+
+                col_num += 2
+                row_num -= 4
+
+        col_num -= 10
+        row_num += 4
+
+        print('__________________________________________________________________________\n'*4)
+    return material_data
+
+
+if __name__ == "__main__":
+    # html_dict = get_html(xpath_resources)
+    # metal_html = html_dict.get('metal')
+    # with open('output.html', 'w', encoding='utf-8') as file:
+    #     file.write(str(metal_html))
+
+    with open('output.html', 'r', encoding='utf-8') as file:
+        metal_html = BeautifulSoup(file.read(), 'lxml')
+
+    print(material_pars(metal_html))
